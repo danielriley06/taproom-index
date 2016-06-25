51517886834322a6bf6e7ea3f0970467b79925a3
danielriley06 <driley3@gmail.com>
2016-06-25 17:51:23 -0400

    Added application_controller and basic setup for it

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "brewery_secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+end
