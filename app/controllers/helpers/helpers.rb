7790bb2d537cc1a25122bb9bae0e53fe34330a82
danielriley06 <driley3@gmail.com>
2016-06-26 18:53:40 -0400

    fixed helpers (had added originally to wrong file)

--- app/controllers/helpers/helpers.rb
+++ app/controllers/helpers/helpers.rb
@@ -1,3 +1,15 @@
-module ApplicationHelper
+module Sinatra
+  module TaproomIndex
+    module Helpers
 
+      def require_logged_in
+        redirect('/sessions/new') unless is_authenticated?
+      end
+
+      def is_authenticated?
+        return !!session[:user_id]
+      end
+
+    end
+  end
 end
