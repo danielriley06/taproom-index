1edc8cc7d65f91ace7223e72a78d899a59418839
danielriley06 <driley3@gmail.com>
2016-06-27 18:12:34 -0400

    added routes to handle signup request in users controller

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to '/clubs'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(:username => params[:username], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect '/bags'
+    end
+  end
+
   get '/login' do
     @error_message = params[:error]
     if !session[:user_id]
