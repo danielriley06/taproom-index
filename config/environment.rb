022542a0b04f221ba68bd07db54f55fcbc22c0f1
danielriley06 <driley3@gmail.com>
2016-07-13 01:43:56 -0400

    fucking heroku dbs

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,15 @@ Bundler.require(:default)
 
 require_all 'app'
 
-ActiveRecord::Base.establish_connection(ENV['production'] || 'postgres://galhabotqupxfg:cIVhlPt4Wsck16uqJYch48YPx2@ec2-54-243-199-161.compute-1.amazonaws.com:5432/d89r24kfi4dmsj')
+configure :production do
+ db = URI.parse(ENV['postgres://galhabotqupxfg:cIVhlPt4Wsck16uqJYch48YPx2@ec2-54-243-199-161.compute-1.amazonaws.com:5432/d89r24kfi4dmsj'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
