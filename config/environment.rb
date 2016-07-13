750f75eb7e3053f40edba82dd3daf040d5a9d3e7
danielriley06 <driley3@gmail.com>
2016-07-13 01:25:16 -0400

    heroku initial test 4

--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,6 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
-#Sets production database to postgreSQL
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 require_all 'app'
